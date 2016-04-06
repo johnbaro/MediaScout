96bfdd203ee9bdec883d765657a69b899dfcdde7
John Baro <johnbaro@gmail.com>
2016-04-06 21:40:06 +1000

    searchTextBox fixes

---
+++ Decompile/MediaScout.GUI.Controls/Themes/Generic.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace themes
+{
+    /// <summary>
+    /// Interaction logic for Generic.xaml
+    /// </summary>
+    public partial class Generic : UserControl
+    {
+        public Generic()
+        {
+            InitializeComponent();
+        }
+    }
+}
